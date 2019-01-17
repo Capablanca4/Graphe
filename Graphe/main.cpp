439b857d3716dd44b878baf8aeec9431280abf61
Capablanca4 <44622746+Capablanca4@users.noreply.github.com>
2019-01-17 15:05:33 +0100

    Ajout de nouvelles fonctions
    Ajout de divers nouvelles fonctions . Il faut tout de fois ajouter des tests unitaires.

--- Graphe/main.cpp
+++ Graphe/main.cpp
@@ -22,10 +22,98 @@ void cdeterminer_aps(int *fs,int nbsommet,int *&aps){
     *(aps+1)=1;
     for(int i=1;i<fs[0]+1;i++){
         if(fs[i]==0){
-            aps[k]=i+1;
+            k[aps]=i+1;
             k++;
         }
     }
+}
+/// On suppose que les vecteur fs et aps sont initialement vide
+void adjVersFs_Aps(const std::vector<std::vector<int>>& M , std::vector<int>& fs, std::vector<int>& aps){
+    int n=M[0][0];
+    int m=M[0][1];
+    fs.push_back(n+m);
+    aps.push_back(n);
+
+    int k=1;
+    for(int i=1;i<n+1;i++){
+        aps.push_back(k);
+        for(int j=1;j<n+1;j++){
+            if(M[i][j]!=0){
+                fs.push_back(j);
+                k++;
+            }
+        }
+    fs.push_back(0);
+    k++;
+    }
+}
+
+/// On suppose la matrice M initialement vide
+void fs_apsVersAdj(const std::vector<int>& fs,const std::vector<int>& aps, std::vector<std::vector<int>>& M){
+    int n = aps[0];
+    int m=fs[0];
+    std::vector<int> A;
+    A.push_back(n);
+    A.push_back(m);
+    M.push_back(A);
+    for(int i=1;i<n+1;i++){
+        std::vector<int> B;
+        for(int j=0;j<n+1;j++){
+            B.push_back(0);
+        }
+        if(i==n){
+            for(int j=aps[n];j<fs[0]-1;j++){
+                B[fs[j]]=1;
+            }
+        }
+        else{
+            for(int j=aps[i];j<aps[i+1]-1;j++){
+                B[fs[j]]=1;
+            }
+        }
+        M.push_back(B);
+    }
+}
+
+void inverseFs_aps(std::vector<int>& fs,int nbsommet){
+    std::vector<std::vector<int>> mat{std::vector<int>{nbsommet}};
+    for(int i=1;i<nbsommet;i++){
+        mat.push_back(std::vector<int>{});
+    }
+    int indicemat=1;
+    int indice=1;
+    while(indice<=fs[0]){
+        if(fs[indice]==0){
+            indicemat++;
+        }
+        else{
+            mat[fs[indice]].push_back(indicemat);
+        }
+        indice++;
+    }
+    indice=1;
+    for(int i=1;i<mat.size();i++){
+        mat[i].push_back(0);
+        for(int j=0;j<mat[i].size();j++){
+            fs[indice++]=mat[i][j];
+        }
+    }
+}
+
+void inverseAdj(std::vector<std::vector<int>>& M){
+    int n=M[0][0];
+    int m=M[0][1];
+    std::vector<std::vector<int>> new_M;
+    new_M.push_back(std::vector<int>{n,m});
+    for(int i=1;i<n+1;i++){
+        new_M.push_back(std::vector<int>{0});
+    }
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<n+1;j++){
+            new_M[j].push_back(M[i][j]);
+        }
+    }
+    M=new_M;
 }
 
 struct ensemble{
@@ -85,17 +173,67 @@ void dist(const std::vector<int>& fs,const std::vector<int>& aps,int sommet,std:
 int main()
 {
     std::vector<int> aps{7,1,5,7,10,13,15,16};
-    std::vector<int> fs{18,2,3,5,0,3,0,3,4,0,6,5,0,3,0,0,4,6,0};
-    std::vector<int> dst{};
-    /*for(int i=0;i<aps[0];i++){
-        dst.push_back(INT_MAX);
-    }
-    dist(fs,aps,1,dst);
-    for(int i=0;i<dst.size();i++){
-        std::cout << dst[i] << std::endl;
+    std::vector<int> fs{18,2,3,5,0,3,0,3,4,0,5,6,0,3,0,0,4,6,0};
+    std::vector<int> aps2{};
+    std::vector<int> fs2{};
+    const std::vector<std::vector<int>> M{ {7,11},
+                                   {0,0,1,1,0,1,0,0},
+                                   {0,0,0,1,0,0,0,0},
+                                   {0,0,0,1,1,0,0,0},
+                                   {0,0,0,0,0,1,1,0},
+                                   {0,0,0,1,0,0,0,0},
+                                   {0,0,0,0,0,0,0,0},
+                                   {0,0,0,0,1,0,1,0} };
+    /*determiner_aps(fs,7,aps2); /// test du fonctionnement de determiner_aps()
+    for(int i=0;i<aps2.size();i++){
+        std::cout<<aps2[i]<<std::endl;
     }*/
 
-    int fs2[19] = {18,2,3,5,0,3,0,3,4,0,6,5,0,3,0,0,4,6,0};
+/*    adjVersFs_Aps(M,fs2,aps2); /// test du fonctionnement de adjVersFs_Aps()
+    std::cout<<"fs : ";
+    for(int i=0;i<fs2.size();i++){
+        std::cout<< fs2[i] << ";";
+    }
+    std::cout<<std::endl;
+    std::cout<<"aps : ";
+    for(int i=0;i<aps2.size();i++){
+        std::cout<< aps2[i] << ";";
+    }
+    std::cout<<std::endl;*/
+
+    /*std::vector<std::vector<int>> M2{}; /// test du fonctionnement de fs_apsVersAdj
+    fs_apsVersAdj(fs,aps,M2);
+    for(int i=0;i<M2.size();i++){
+        for(int j=0;j<M2[i].size();j++){
+            std::cout << M2[i][j] << " ";
+        }
+        std::cout <<std::endl;
+    }*/
+
+    /*std::vector<int> fs3{10,2,3,0,4,0,4,0,0,4,0}; /// Test du fonctionnement de inverseFs_aps
+    inverseFs_aps(fs3,5);
+    for(int i=0;i<fs3.size();i++){
+        std::cout << fs3[i] << std::endl;
+    }*/
+
+    /*std::vector<std::vector<int>> M2{ {7,11},
+                                   {0,0,1,1,0,1,0,0},
+                                   {0,0,0,1,0,0,0,0},
+                                   {0,0,0,1,1,0,0,0},
+                                   {0,0,0,0,0,1,1,0},
+                                   {0,0,0,1,0,0,0,0},
+                                   {0,0,0,0,0,0,0,0},
+                                   {0,0,0,0,1,0,1,0} };
+
+    inverseAdj(M2); /// Test du fonctionnement de inverseAdj
+    for(int i=0;i<M2.size();i++){
+        for(int j=0;j<M2[i].size();j++){
+            std::cout << M2[i][j] << " ";
+        }
+        std::cout <<std::endl;
+    }*/
+
+    /*int fs2[19] = {18,2,3,5,0,3,0,3,4,0,6,5,0,3,0,0,4,6,0};/// programme
     int *pfs;
     // La référence sur un pointeur doit être initialisée ! On crée un int * pour staisfaire lecompilateur
     int *&aps2 = pfs;
@@ -103,7 +241,7 @@ int main()
     cdeterminer_aps(fs2,7,aps2);
     for(int i=0;i<aps2[0];i++){
         std::cout << aps2[i] << std::endl;
-    }
+    }*/
 
     return 0;
 }
